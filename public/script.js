b07bc5c46346b4218a815c72a830e2ebe43a78d2
nosianuri <nurinosia@gmail.com>
2022-07-17 18:54:21 +0600

    set errors

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
-const socket = io('/')
-const videoGrid = document.getElementById('video-grid')
+const socket = io('/');
+const videoGrid = document.getElementById('video-grid');
 const myPeer = new Peer(undefined, {
   path: '/peerjs',
   host: '/',
@@ -14,7 +14,8 @@ navigator.mediaDevices.getUserMedia({
   audio: true
 }).then(stream => {
   myVideoStream = stream;
-  addVideoStream(myVideo, stream)
+  addVideoStream(myVideo, stream);
+
   myPeer.on('call', call => {
     call.answer(stream)
     const video = document.createElement('video')
@@ -63,7 +64,7 @@ function connectToNewUser(userId, stream) {
 }
 
 function addVideoStream(video, stream) {
-  video.srcObject = stream
+  video.srcObject = stream;
   video.addEventListener('loadedmetadata', () => {
     video.play()
   })
